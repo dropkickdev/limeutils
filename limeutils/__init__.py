df2ae017711bdbafcb6512f2c285ca4f50019d99
enchance <enchance@gmail.com>
2021-01-05 20:59:13 +0800

    Bump up 0.1.6 -> 0.1.7

--- limeutils/__init__.py
+++ limeutils/__init__.py
@@ -1,5 +1,5 @@
 from .utils import *
 
 
-__version__ = "0.1.6"
+__version__ = "0.1.7"
 __author__ = 'enchance'
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="limeutils", # Replace with your own username
-    version="0.1.6",
+    version="0.1.7",
     author="enchance",
     author_email="enchance@gmail.com",
     description="A collection of my utility functions",
