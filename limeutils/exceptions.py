9f5e504cbb69ac984f6821e3fd67ffe2ff56fd42
enchance <enchance@gmail.com>
2021-03-20 16:41:56 +0800

    Create exceptions file

--- limeutils/exceptions.py
+++ limeutils/exceptions.py
@@ -1,9 +1,14 @@
 
 
 class ValidationError(Exception):
-    def __init__(self, message=''):
-        self.message = message
-        super().__init__(message)
+    def __init__(self, message='', literal: list = None):
+        if literal:
+            literal = literal and [f'"{i}"' for i in literal] or []
+            items = literal and ' or '.join(literal) or ''
+            self.message = message or f'Arguments can only be: {items}.'
+        else:
+            self.message = message or 'Arguments must use the correct value or selection of values.'
+        super().__init__(self.message)
 
 
 class RedisError(Exception):
