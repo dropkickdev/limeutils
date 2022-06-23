98fa57edafec316b91a711ba0dd483c33f9d9426
enchance <enchance@gmail.com>
2022-06-23 23:01:02 +0800

    modstr accepts multiple fields

--- limeutils/db.py
+++ limeutils/db.py
@@ -13,9 +13,20 @@ def model_str(instance, attr) -> str:
            or f'<{instance.__class__.__name__}: {instance.id}>'
 
 
-def modstr(instance, attr) -> str:
-    """Alias for model_str()"""
-    return model_str(instance, attr)
+# TESTME: Untested
+def modstr(instance, *attr) -> str:
+    """
+    The field to display for an object's __str__. If the field doesn't exist then an
+    alternative will be displayed.
+    :param instance:    Instance object
+    :param *attr:       Field/s name to get data from if it exists
+    :return:            str
+    """
+    clsname = instance.__class__.__name__
+    ll = [i for i in attr if hasattr(instance, i) and getattr(instance, i)]
+    if ll:
+        return f'<{clsname} {instance.id}: {", ".join(ll)}>'
+    return f'<{clsname}: {instance.id}>'
 
 
 def classgrabber(dotpath: str):
