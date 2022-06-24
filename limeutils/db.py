d73622b165c4aea7a4c62ee9ab4f0085432de84c
enchance <enchance@gmail.com>
2022-06-24 14:05:40 +0800

    update to modstr()

--- limeutils/db.py
+++ limeutils/db.py
@@ -14,19 +14,23 @@ def model_str(instance, attr) -> str:
 
 
 # TESTME: Untested
-def modstr(instance, *attr) -> str:
+def modstr(instance, *attr, onlyid: bool = False) -> str:
     """
     The field to display for an object's __str__. If the field doesn't exist then an
     alternative will be displayed.
     :param instance:    Instance object
     :param *attr:       Field/s name to get data from if it exists
+    :param onlyid:      Only return the id
     :return:            str
     """
     clsname = instance.__class__.__name__
-    ll = [i for i in attr if hasattr(instance, i) and getattr(instance, i)]
-    if ll:
+    ll = [getattr(instance, i) for i in attr if hasattr(instance, i) and getattr(instance, i)]
+    try:
+        if onlyid or not ll:
+            return f'<{clsname}: {instance.id}>'
         return f'<{clsname} {instance.id}: {", ".join(ll)}>'
-    return f'<{clsname}: {instance.id}>'
+    except AttributeError:
+        return f'<{clsname}>'
 
 
 def classgrabber(dotpath: str):
