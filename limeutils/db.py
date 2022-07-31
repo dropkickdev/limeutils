7dd847fabd991ead2eddc785d0cae2e3580fd98c
enchance <enchance@gmail.com>
2022-07-31 13:49:21 +0800

    code cleanup

--- limeutils/db.py
+++ limeutils/db.py
@@ -1,6 +1,8 @@
 import importlib
 from typing import Optional, Any, List
 
+from icecream import ic
+
 
 def model_str(instance, attr) -> str:
     """
@@ -15,13 +17,14 @@ def model_str(instance, attr) -> str:
 
 
 # TESTME: Untested
-def modstr(instance, *attr, data: Optional[List[Any]] = None, onlyid: bool = False) -> str:
+def modstr(instance, *attr: str, data: Optional[List[Any]] = None,
+           onlyid: Optional[bool] = False) -> str:
     """
     The field to display for an object's __str__. If the field doesn't exist then an
     alternative will be displayed.
     :param instance:    Instance object
-    :param *attr:       Field/s name to get data from if it exists
-    :param data:        Any data not
+    :param attr:        Field/s name to get data from if it exists
+    :param data:        Any data that's not a field
     :param onlyid:      Only return the id
     :return:            str
     """
@@ -29,6 +32,7 @@ def modstr(instance, *attr, data: Optional[List[Any]] = None, onlyid: bool = Fal
     data = data or []
     ll = [getattr(instance, i) for i in attr if hasattr(instance, i) and getattr(instance, i)]
     ll += [i for i in data if i]
+    
     try:
         if onlyid or not ll:
             return f'<{clsname}: {instance.id}>'
